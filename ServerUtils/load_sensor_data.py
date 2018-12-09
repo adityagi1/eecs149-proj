fbeb70b895d9d1b2f706a9416c94376bd26e9904
Tanner <tdekock@berkeley.edu>
2018-12-08 20:03:08 -0800

    updated server utils

--- ServerUtils/load_sensor_data.py
+++ ServerUtils/load_sensor_data.py
@@ -2,9 +2,10 @@ import json, sys, csv, datetime, time
 import paho.mqtt.client as mqtt
 from jsonschema import validate
 
+#recieving one sensor value and setpoint(possibly empty) at a time
 schema_file = "dout_header.json" #defalut schema
 subscription_list_file = "Topics\sub_list.txt"
-hostname = "agrasp.wifizone.org"
+hostname = "agrasp.wifizone.org" #80, topic: test
 file_to_write = "" 
 timestamp = "" #datetime.datetime.now()
 valid_json = False
@@ -21,56 +22,74 @@ def setup_client():
 	
 def check_header(data, file):
 	try:
-		with open(file, 'r+') as read_file:
+		with open(file, 'r') as read_file:
 			reader = csv.reader(read_file)
-			if (len(reader[0]) == 0 or len(reader[0]) != len(data["data"])):
-				print("Header is not up-to-date\n")
-				read_file.close()
-				update_header(data, read_file)
-		read_file.close()
+			data_label = str(data["data"][0]["d_label"])
+			header_row = next(reader)
+			if (len(header_row) == 0 or data_label not in header_row ):
+				print("No mathcing data type found")
+				return False
+			read_file.close()
 		return True
 	except IOError:
 		print("Could not open " + file + "\n")
 		return False
 	return False
 
-def updateheader(data, file):
-	print("Updating header")
-	header_row = [pt["d_label"] for pt in data["data"]]
-	header_row.insert(0, "Timestamp")
-	writer = csv.writer(file)
-	writer.writerow(header_row)
-	return True
-
 def parse_data(data, file):
-	row = [pt["d_value"] for pt in data["data"]]
-	row.insert(0, timestamp)
+	timestamp = datetime.datetime.now()
+	write_data = data["data"][0]["d_value"]
+	row = [timestamp, write_data]
+	#row.insert(0, timestamp)
 	try:
-		with open(file, 'w') as wf:
-			writer = csv.writer(file)
+		with open(file, 'a', newline='') as wf:
+			writer = csv.writer(wf)
 			writer.writerow(row)
-		wf.close()
+			wf.close()
 		return True
 	except IOError:
 		print("Could not write to " + file + "\n")
 		return False
 	return False
 	
-def check_setpoint():
+def check_setpoint(data):
+	#compare header of setpoint
+	spt_file = "setpoints\\" + str(device_id) + "_setpoint.csv"
+	if len(data["setpoint"]) == 0:
+		print("This data type has no setpoint")
+		return True
+	data_spt_header = data["setpoint"][0]["d_label"]
+	data_spt_val = data["setpoint"][0]["d_value"]
+	try:
+		#compare setpoint values
+		with open(spt_file, 'r+') as f:
+			csv_reader = csv.reader(f)
+			header_row = next(csv_reader)
+			indx = header_row.index(data_spt_header)
+			data_row = next(csv_reader)
+			if (data_row[indx] != data_spt_val):
+				print("Mismatch b/w setpoitns, sending correct setpoint")
+				pub_topic = user + "/" + data["device_id"] + "/" + "setpoint/" + data["device_type"]
+				payload_to_send = {"device_id":data["device_id"], "device_type":data["device_type"], "setpoint":[{"d_label":header_row[indx],"d_value":data_row[indx]}]}
+				client.publish(pub_topic,json.dumps(payload_to_send).encode(),1)
+			f.close()
+	except IOError:
+		print("Could not check " + spt_file + "\n")
 	return True
 	
-def update_sub_list():
+def update_sub_list(client):
 	print("Checking for updates in subscription list")
 	try:
-		with open(subscription_list_file, 'r') as f:
+		with open(subscription_list_file, 'r+') as f:
 			for line in f:
+				print(line + "\n")
 				sub_list.append(line)
-		f.close()
+			f.close()
+			for topic in sub_list:
+				client.subscribe(topic,0)
 	except IOError:
 		print("Could not open " + subscription_list_file + "\n")
 		client.loop_stop()
-	for topic in sub_list:
-		client.subscribe(topic)
 
 def on_connect(client, userdata, flags, rc):
 	if rc == 0:
@@ -82,23 +101,27 @@ def on_message(client, userdata, msg):
 	topic = msg.topic
 	m_decode = str(msg.payload.decode("utf-8", "ignore"))
 	json_in = json.loads(m_decode)
+	user = topic.split()[0]
 	try:
-		with open(schema, 'r') as s:
+		with open(schema, 'r+') as s:
 			valid_json = validate(json_in, s)
-	except IOError:
+	except IOError:	
 		print("Could not open " + schema_file + "/n")
 	if(valid_json): 
-		file_to_write = str("Sensor Data/" + data_out[device_id]) + ".csv"
-		check_header(json_in, file_to_write)
-		parse_data(json_in, file_to_write)
+		file_to_write = "sensor_data\\" + str(data_out[device_id]) + "\\" + data["data"][0]["d_label"] + ".csv"
+		if check_header(json_in, file_to_write):
+			check_setpoint(json_in)
+			parse_data(json_in, file_to_write)
+		else:
+			print("Type of data provided did not match type in csv file")
 	else:
 		print("JSON recieved was not of the correct format")
 	
 def main():
 	client = setup_client()
 	while True:
+		update_sub_list(client)
 		client.loop_start()
-		update_sub_list()
 		time.sleep(5)
 		client.loop_stop()
 		
--- ServerUtils/update_sensor_sub_list.py
+++ ServerUtils/update_sensor_sub_list.py
@@ -6,26 +6,28 @@ new_topics = []
 
 def check_file(topic):
 	try:
-		with open(subscription_list_file, 'r') as f:
-			for line in f:
-				if line == topic:
+		with open(subscription_list_file, 'r+') as f:
+			lines = [line.rstrip('\n') for line in f]
+			for line in lines:
+				if str(line) == topic:
+					print("Topic: " + topic + " was already present\n")
 					return False
-		f.close()
+			f.close()
 		return True
 	except IOError:
-		print("Could not open" + subscription_list_file + "\n")
-	return False
+		print("Could not open " + subscription_list_file + "\n")
+	return True
 	
 def main():
 	try:
-		with open(subscription_list_file, 'a') as f:
+		with open(subscription_list_file, 'a+') as f:
 			f.writelines(new_topics)
-		f.close()
+			f.close()
 	except IOError:
 		print("Could not open " + subscription_list_file + "\n")
 
 if __name__ == "__main__": 
 	for x in range(1,len(sys.argv)):
-		if check_file(x):
-			new_topics.append(sys.argv[x])
+		if check_file(sys.argv[x]):
+			new_topics.append(str(sys.argv[x] + "\n"))
 	main()
